949d23c33fd172847fdf1429dc4d21aaa4b86efe
DarioRodriguez47 <lenin.rodriguez@epn.edu.ec>
2025-08-05 18:22:01 -0500

    fix: redirección automática a /productos como página principal

--- frontend-productos/src/App.js
+++ frontend-productos/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import ProductoList from "./components/ProductoList";
 import ProductoForm from "./components/ProductoForm";
 import ProductoDetail from "./components/ProductoDetail";
@@ -19,7 +19,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/productos" element={<AppleProducts/>} />
-      <Route path="/" element={<ProductoList />} />
+      <Route path="/" element={<Navigate to="/productos" />} />
       <Route path="/crear-producto" element={<ProductoForm fetchProductos={fetchProductos} />} />
       <Route path="/producto/:id" element={<ProductoDetail />} />
       <Route path="/producto/editar/:id" element={<ProductoEdit />} />
